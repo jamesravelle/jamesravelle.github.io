60dc1d6fcec93113beb8ec12acf6ab5236f9f712
jxrvll <jxrvll@gmail.com>
2020-07-17 17:26:05 -0400

    Update portfolio

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 $( document ).ready(function() {
 
+    var i = 0;
+    var txt = 'About Me';
+    var speed = 100; 
 
+function typeWriter() {
+  if (i < txt.length) {
+    $("#about-header").append(txt[i])
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}   
 
+    $( "#browser-window" ).toggle( "slide", function(){
+        typeWriter();
+    });
 
 
 
